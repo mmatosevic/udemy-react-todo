7b336e9cc8026db7f0848ad5d40a7e0d4925c627
Matija Matošević <matija.matosevic@infobip.com>
2017-10-17 22:09:33 +0200

    Added tests for components and list.

---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var expect = require("expect");
+var $ = require('jQuery');
+
+var TodoList = require('TodoList');
+var Todo = require('Todo');
+
+describe('TodoList', () => {
+    it('Should exist', () => {
+        expect(TodoList).toExist();
+    });
+
+    it('Should render one Todo component for each todo item', () => {
+        var todos = [
+            {
+                "id": 1,
+                "text": "Do something"
+            },
+            {
+                "id": 2,
+                "text": "Check mail"
+            }
+        ];
+        var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+        var todosComponenets = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+        expect(todosComponenets.length).toBe(2);
+    });
+});
